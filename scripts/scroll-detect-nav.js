ccbb18fa0a43800ad9507927c265cb53bcd4e841
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2024-07-09 22:40:33 +0800

    feat: hamburger menu button

--- scripts/scroll-detect-nav.js
+++ scripts/scroll-detect-nav.js
@@ -1,11 +1,17 @@
+// Hide hamburger on initial load
+$(document).ready(function() {
+    $('.hamburger').addClass('hide');
+});
+
+// Collapse navbar into hamburger
 $(document).scroll(function() {
     if ($(document).scrollTop() >= 30) {
-        $('.front-page-nav').addClass('scroll-down');
-        $('.front-page-link').hide();
-        $('#front-page-color-select').hide();
+        $('.front-page-nav').addClass(['scroll-down', 'hide']);
+        $('.hamburger').removeClass('hide');
+        $('.hamburger').addClass('scroll-down');
     } else {
-        $('.front-page-nav').removeClass('scroll-down');
-        $('.front-page-link').show();
-        $('#front-page-color-select').show();
+        $('.hamburger').removeClass('scroll-down');
+        $('.hamburger').addClass('hide');
+        $('.front-page-nav').removeClass(['scroll-down', 'hide']);
     }
 });
