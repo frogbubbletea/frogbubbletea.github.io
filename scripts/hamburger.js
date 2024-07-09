4b542e1187e6bb192d0db471e50bea12548a8385
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2024-07-10 00:08:21 +0800

    feat: apply hamburger menu to projects page

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -4,8 +4,3 @@ function hamburgerOpen() {
     $('.hamburger').removeClass('scroll-down');
     $('.hamburger').addClass('hide')
 }
-
-function hamburgerClose() {
-    $('.front-page-nav').removeClass('in-menu');
-    $('.front-page-nav').addClass(['scroll-down', 'hide']);
-}
